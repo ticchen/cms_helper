45725158f67245b2421e902e7e9ca727e05d4796
tic_chen <tic_chen@gemteks.com>
2014-11-10 02:01:00 +0800

    astyle source code: -A8 --indent=tab -p

--- cms.c
+++ cms.c
@@ -9,63 +9,63 @@ extern char **environ;
 
 void env_dump()
 {
-	int i=0;
-	
-	if(environ==NULL){
+	int i = 0;
+
+	if(environ == NULL) {
 		return;
 	}
-	
-	while( environ[i] != NULL ){
+
+	while( environ[i] != NULL ) {
 		printf("%s\r\n", environ[i]);
 		i++;
 	}
-	
+
 	return;
 }
 
 
 int cms_init()
 {
-	static cms_init_flag=0;
-	
-	if(cms_init_flag){
-		return 0; //init already 
+	static cms_init_flag = 0;
+
+	if(cms_init_flag) {
+		return 0; //init already
 	}
-	
+
 	//flush origin env
 	clearenv();
-	cms_init_flag =1;
+	cms_init_flag = 1;
 	return 0;
 }
 
 void cms_config_import(char *config_file)
 {
-	if(config_file == NULL){
+	if(config_file == NULL) {
 		return;
 	}
-	
-	FILE *fp=fopen(config_file,"r");
-	if(fp == NULL){
+
+	FILE *fp = fopen(config_file, "r");
+	if(fp == NULL) {
 		return;
 	}
-	
-	char buff[80]={0};
-	char *next=NULL;
-	char *start=NULL;
-	while( (start=fgets(buff,sizeof(buff),fp))!=NULL){
-		if(start[0]=='#'){
+
+	char buff[80] = {0};
+	char *next = NULL;
+	char *start = NULL;
+	while( (start = fgets(buff, sizeof(buff), fp)) != NULL) {
+		if(start[0] == '#') {
 			continue;
 		}
-		start=strsep(&start,"\n");	//remove tailing newline
-		
+		start = strsep(&start, "\n");	//remove tailing newline
+
 		//seperate =
-		next=start;
-		start=strsep(&next,"=");
-		
+		next = start;
+		start = strsep(&next, "=");
+
 		//printf("%s=%s\n",start,next);
-		setenv(start,next,1);
+		setenv(start, next, 1);
 	}
-	
+
 	fclose(fp);
 }
 
@@ -75,21 +75,21 @@ char *cms_getenv(const char *name)
 
 	cms_init();
 
-	if( name == NULL ){
+	if( name == NULL ) {
 		return NULL;
 	}
-		
+
 	char *value = getenv(name);
-	if(value == NULL){
+	if(value == NULL) {
 		return NULL;
 	}
-	
-	if(strlen(value) == 0){
+
+	if(strlen(value) == 0) {
 		return NULL;	//treat empty string as NULL
 	}
 
 	memset(value_buff, 0x00, BUFFER_MAX);
-	strncpy(value_buff, value, BUFFER_MAX-1);
+	strncpy(value_buff, value, BUFFER_MAX - 1);
 	return value_buff;
 }
 
@@ -97,18 +97,18 @@ int cms_setenv(const char *name, const char *value)
 {
 	cms_init();
 
-	if( name == NULL || value == NULL){
+	if( name == NULL || value == NULL) {
 		return -1;
 	}
-	
-	char *origin_value=getenv(name);
-	int ret=0;
-	if( origin_value && strcmp(origin_value,value)==0 ){
-		ret=0; //value not change
-	}else{
-		ret=1; //value changed
+
+	char *origin_value = getenv(name);
+	int ret = 0;
+	if( origin_value && strcmp(origin_value, value) == 0 ) {
+		ret = 0; //value not change
+	} else {
+		ret = 1; //value changed
 	}
-	setenv(name,value,1);
-	
+	setenv(name, value, 1);
+
 	return ret;
 }
--- cms_helper.c
+++ cms_helper.c
@@ -13,17 +13,17 @@
  **************************************/
 long int strtol_default(const char *nptr, char **endptr, int base, long int default_value)
 {
-	if(nptr == NULL){
+	if(nptr == NULL) {
 		return default_value;
 	}
-	
+
 	errno = 0;
 	long int value = strtol(nptr, endptr, base);
 	if ((errno == ERANGE && (value == LONG_MIN || value == LONG_MAX))	//range error
-		|| (errno != 0 && value == 0)) {	//format error
+	    || (errno != 0 && value == 0)) {	//format error
 		return default_value;
 	}
-	
+
 	if (*endptr == nptr) {
 		//fprintf(stderr, "No digits were found\n");
 		return default_value;
@@ -34,15 +34,15 @@ long int strtol_default(const char *nptr, char **endptr, int base, long int defa
 
 unsigned long int strtoul_default(const char *nptr, char **endptr, int base, unsigned long int default_value)
 {
-	if(nptr == NULL){
+	if(nptr == NULL) {
 		return default_value;
 	}
 
 	errno = 0;
-	char *neg_sign=strchr(nptr,'-');
-	if(neg_sign){
+	char *neg_sign = strchr(nptr, '-');
+	if(neg_sign) {
 		//found a negative sign
-		if( (unsigned long int)(neg_sign-nptr)+1 == strcspn(nptr,"1234567890") ){
+		if( (unsigned long int)(neg_sign - nptr) + 1 == strcspn(nptr, "1234567890") ) {
 			//the negative sign is at the beginning of digital. // ex: "   -1234"
 			return default_value;
 		}
@@ -50,63 +50,50 @@ unsigned long int strtoul_default(const char *nptr, char **endptr, int base, uns
 
 	unsigned long int value = strtoul(nptr, endptr, base);
 	if ((errno == ERANGE && value == ULONG_MAX ) //range error
-		|| (errno != 0 && value == 0 )) {	//format error
+	    || (errno != 0 && value == 0 )) {	//format error
 		return default_value;
 	}
-	
+
 	if (*endptr == nptr) {
 		//fprintf(stderr, "No digits were found\n");
 		return default_value;
 	}
-		
+
 	return value;
 }
 
 
 static char *cms_get_str_valist(const char *format, char *default_value, va_list args)
 {
-	if(format == NULL){
+	if(format == NULL) {
 		return default_value;
 	}
-	
+
 	const char *name = format;
-	if(strchr(format,'%') != NULL){
-		name=alloca(MaxStringLen);
+	if(strchr(format, '%') != NULL) {
+		name = alloca(MaxStringLen);
 		int len = vsnprintf((char *)name, MaxStringLen, format, args);
-		if(len < 0 || MaxStringLen <= len){ \
+		if(len < 0 || MaxStringLen <= len) {
+			\
 			return default_value;
-		} 
+		}
 	}
-	
-	char *value_str=CMSGetValue(name,GetValueUser);
-	if(value_str == NULL){
+
+	char *value_str = CMSGetValue(name, GetValueUser);
+	if(value_str == NULL) {
 		return default_value;
 	}
-	
+
 	return value_str;
 }
 
 
-char *cms_get_str(const char *format, char *default_value, ...)
-{
-	if(format == NULL){
-		return default_value;
-	}
-
-	va_list args;
-	va_start(args, default_value);
-	char *value_str = cms_get_str_valist(format, default_value, args);
-	va_end(args);
-	return value_str;
-}	
-
-
 int cms_get_int(const char *format, int default_value, ...)
 {
-	if(format == NULL){
+	if(format == NULL) {
 		return default_value;
 	}
-	
+
 	va_list args;
 	va_start(args, default_value);
 	char *value_str = cms_get_str_valist(format, NULL, args);
@@ -119,7 +106,7 @@ int cms_get_int(const char *format, int default_value, ...)
 
 unsigned int cms_get_uint(const char *format, unsigned int default_value, ...)
 {
-	if(format == NULL){
+	if(format == NULL) {
 		return default_value;
 	}
 
@@ -133,22 +120,35 @@ unsigned int cms_get_uint(const char *format, unsigned int default_value, ...)
 }
 
 
+char *cms_get_str(const char *format, char *default_value, ...)
+{
+	if(format == NULL) {
+		return default_value;
+	}
+
+	va_list args;
+	va_start(args, default_value);
+	char *value_str = cms_get_str_valist(format, default_value, args);
+	va_end(args);
+	return value_str;
+}
+
 
 static char *cms_get_str_index_valist(const char *format, char *default_value, unsigned int index, char *separator, va_list args)
 {
-	if(format == NULL || separator == NULL){
+	if(format == NULL || separator == NULL) {
 		return default_value;
 	}
 
 	char *value_str = cms_get_str_valist(format, NULL, args);
-	if(value_str == NULL){
+	if(value_str == NULL) {
 		return default_value;
 	}
 
-	unsigned int i=0;
-	char *token=NULL;
-	for(i = 0, token = strsep(&value_str, separator); i <= index && token != NULL; i++, token = strsep(&value_str, separator)){
-		if(i == index){
+	unsigned int i = 0;
+	char *token = NULL;
+	for(i = 0, token = strsep(&value_str, separator); i <= index && token != NULL; i++, token = strsep(&value_str, separator)) {
+		if(i == index) {
 			return token;
 		}
 	}
@@ -156,9 +156,41 @@ static char *cms_get_str_index_valist(const char *format, char *default_value, u
 }
 
 
-char *cms_get_str_index(const char *format, char *default_value,unsigned int index, char *separator, ...)
-{	
-	if(format == NULL || separator == NULL){
+int cms_get_int_index(const char *format, int default_value, unsigned int index, char *separator, ...)
+{
+	if(format == NULL || separator == NULL) {
+		return default_value;
+	}
+
+	va_list args;
+	va_start(args, separator);
+	char *value_str = cms_get_str_index_valist(format, NULL, index, separator, args);
+	va_end(args);
+
+	char *endptr = NULL;
+	return strtol_default(value_str, &endptr, 10, default_value);
+}
+
+
+unsigned int cms_get_uint_index(const char *format, unsigned int default_value, unsigned int index, char *separator, ...)
+{
+	if(format == NULL || separator == NULL) {
+		return default_value;
+	}
+
+	va_list args;
+	va_start(args, separator);
+	char *value_str = cms_get_str_index_valist(format, NULL, index, separator, args);
+	va_end(args);
+
+	char *endptr = NULL;
+	return strtoul_default(value_str, &endptr, 10, default_value);
+}
+
+
+char *cms_get_str_index(const char *format, char *default_value, unsigned int index, char *separator, ...)
+{
+	if(format == NULL || separator == NULL) {
 		return default_value;
 	}
 
@@ -171,114 +203,37 @@ char *cms_get_str_index(const char *format, char *default_value,unsigned int ind
 }
 
 
-int cms_get_int_index(const char *format, int default_value, unsigned int index, char *separator, ...)
-{
-	if(format == NULL || separator == NULL){
-		return default_value;
-	}
-
-	va_list args;
-	va_start(args, separator);
-	char *value_str = cms_get_str_index_valist(format, NULL, index, separator, args);
-	va_end(args);
-
-	char *endptr=NULL;
-	return strtol_default(value_str, &endptr, 10, default_value);
-}
-
-
-unsigned int cms_get_uint_index(const char *format, unsigned int default_value, unsigned int index, char *separator, ...)
-{
-	if(format == NULL || separator == NULL){
-		return default_value;
-	}
-
-	va_list args;
-	va_start(args, separator);
-	char *value_str = cms_get_str_index_valist(format, NULL, index, separator, args);
-	va_end(args);
-
-	char *endptr=NULL;
-	return strtoul_default(value_str, &endptr, 10, default_value);
-}
-
-
-int cms_get_str_array(const char *format, char *default_value, const char *array[], size_t array_size, char *get_buffer, size_t get_buffer_size, char *separator, ...)
-{
-	int i=0;
-	int token_num=0;
-
-	va_list args;
-	va_start(args, separator);
-	char *value_str = cms_get_str_valist(format, NULL, args);
-	va_end(args);
-
-	int is_array_exist=0;
-	if(array != NULL && array_size !=0 && get_buffer != NULL && get_buffer_size !=0 ){
-		is_array_exist=1;
-	}
-
-	if( value_str != NULL){
-		if(get_buffer !=NULL && get_buffer_size != 0){
-			snprintf(get_buffer, get_buffer_size , "%s", value_str);
-		}else{
-			get_buffer = value_str;
-			get_buffer_size = BUFFER_MAX;
-		}
-		
-		char *token=NULL ,*last_token = NULL;
-		for(token_num=0, token = strsep(&get_buffer, separator); token != NULL; token_num++, token = strsep(&get_buffer, separator)){
-			last_token = token;
-			if(is_array_exist && token_num<array_size){
-				array[token_num] = token;
-			}
-		}
-		if(last_token != NULL && strlen(last_token) == 0){
-			token_num--; //tailing zero-length token is not a real token. 
-		}
-	}
-
-	if(is_array_exist){
-		for(i=token_num;i<array_size;i++){
-			array[i] = default_value;
-		}
-	}
-
-	return token_num;
-}
-
-
 int cms_get_int_array(const char *format, int default_value, int array[], size_t array_size, char *separator, ...)
 {
-	int i=0;
-	int token_num=0;
+	int i = 0;
+	int token_num = 0;
 
 	va_list args;
 	va_start(args, separator);
 	char *value_str = cms_get_str_valist(format, NULL, args);
 	va_end(args);
 
-	int is_array_exist=0;
-	if(array != NULL && array_size !=0){
-		is_array_exist=1;
+	int is_array_exist = 0;
+	if(array != NULL && array_size != 0) {
+		is_array_exist = 1;
 	}
 
-	if( value_str != NULL){
-		char *token=NULL ,*last_token = NULL;
-		for(token_num=0, token = strsep(&value_str, separator); token != NULL; token_num++, token = strsep(&value_str, separator)){
+	if( value_str != NULL) {
+		char *token = NULL , *last_token = NULL;
+		for(token_num = 0, token = strsep(&value_str, separator); token != NULL; token_num++, token = strsep(&value_str, separator)) {
 			last_token = token;
-			if(is_array_exist && token_num<array_size){
+			if(is_array_exist && token_num < array_size) {
 				char *endptr = NULL;
-				array[token_num] = strtol_default(token,&endptr, 10, default_value);
+				array[token_num] = strtol_default(token, &endptr, 10, default_value);
 			}
 		}
-		if(last_token != NULL && strlen(last_token) == 0){
-			token_num--; //tailing zero-length token is not a real token. 
+		if(last_token != NULL && strlen(last_token) == 0) {
+			token_num--; //tailing zero-length token is not a real token.
 		}
 	}
 
-	if(is_array_exist){
-		for(i=token_num;i<array_size;i++){
+	if(is_array_exist) {
+		for(i = token_num; i < array_size; i++) {
 			array[i] = default_value;
 		}
 	}
@@ -288,35 +243,80 @@ int cms_get_int_array(const char *format, int default_value, int array[], size_t
 
 int cms_get_uint_array(const char *format, unsigned int default_value, int array[], size_t array_size, char *separator, ...)
 {
-	int i=0;
-	int token_num=0;
+	int i = 0;
+	int token_num = 0;
 
 	va_list args;
 	va_start(args, separator);
 	char *value_str = cms_get_str_valist(format, NULL, args);
 	va_end(args);
 
-	int is_array_exist=0;
-	if(array != NULL && array_size !=0){
-		is_array_exist=1;
+	int is_array_exist = 0;
+	if(array != NULL && array_size != 0) {
+		is_array_exist = 1;
 	}
 
-	if( value_str != NULL){
-		char *token=NULL ,*last_token = NULL;
-		for(token_num=0, token = strsep(&value_str, separator); token != NULL; token_num++, token = strsep(&value_str, separator)){
+	if( value_str != NULL) {
+		char *token = NULL , *last_token = NULL;
+		for(token_num = 0, token = strsep(&value_str, separator); token != NULL; token_num++, token = strsep(&value_str, separator)) {
 			last_token = token;
-			if(is_array_exist && token_num<array_size){
+			if(is_array_exist && token_num < array_size) {
 				char *endptr = NULL;
-				array[token_num] = strtoul_default(token,&endptr, 10, default_value);
+				array[token_num] = strtoul_default(token, &endptr, 10, default_value);
 			}
 		}
-		if(last_token != NULL && strlen(last_token) == 0){
-			token_num--; //tailing zero-length token is not a real token. 
+		if(last_token != NULL && strlen(last_token) == 0) {
+			token_num--; //tailing zero-length token is not a real token.
 		}
 	}
 
-	if(is_array_exist){
-		for(i=token_num;i<array_size;i++){
+	if(is_array_exist) {
+		for(i = token_num; i < array_size; i++) {
+			array[i] = default_value;
+		}
+	}
+
+	return token_num;
+}
+
+
+int cms_get_str_array(const char *format, char *default_value, const char *array[], size_t array_size, char *get_buffer, size_t get_buffer_size, char *separator, ...)
+{
+	int i = 0;
+	int token_num = 0;
+
+	va_list args;
+	va_start(args, separator);
+	char *value_str = cms_get_str_valist(format, NULL, args);
+	va_end(args);
+
+	int is_array_exist = 0;
+	if(array != NULL && array_size != 0 && get_buffer != NULL && get_buffer_size != 0 ) {
+		is_array_exist = 1;
+	}
+
+	if( value_str != NULL) {
+		if(get_buffer != NULL && get_buffer_size != 0) {
+			snprintf(get_buffer, get_buffer_size , "%s", value_str);
+		} else {
+			get_buffer = value_str;
+			get_buffer_size = BUFFER_MAX;
+		}
+
+		char *token = NULL , *last_token = NULL;
+		for(token_num = 0, token = strsep(&get_buffer, separator); token != NULL; token_num++, token = strsep(&get_buffer, separator)) {
+			last_token = token;
+			if(is_array_exist && token_num < array_size) {
+				array[token_num] = token;
+			}
+		}
+		if(last_token != NULL && strlen(last_token) == 0) {
+			token_num--; //tailing zero-length token is not a real token.
+		}
+	}
+
+	if(is_array_exist) {
+		for(i = token_num; i < array_size; i++) {
 			array[i] = default_value;
 		}
 	}
